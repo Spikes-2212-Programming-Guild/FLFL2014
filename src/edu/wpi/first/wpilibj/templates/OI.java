61f01850207bef11cc9be052e185d4d729e42df5
AtidSpikes <AtidSpikes@Spikes-PC>
2014-11-07 11:39:55 +0200

    modified OI after merge (netbeans plz)

--- src/edu/wpi/first/wpilibj/templates/OI.java
+++ src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,4 +1,3 @@
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -30,56 +29,48 @@ public class OI {
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
+
     // Another type of button you can create is a DigitalIOButton, which is
     // a button or switch hooked up to the cypress module. These are useful if
     // you want to build a customized operator interface.
     // Button button = new DigitalIOButton(1);
-    
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
-    
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
     // three ways:
-    
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
-    
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
-    Joystick joy2 = new Joystick(RobotMap.joystick2Port);    
-    Button elUp = new JoystickButton(joy2,2);
-    Button elDown = new JoystickButton(joy2,3);
+    Joystick joy2 = new Joystick(RobotMap.joystick2Port);
+    Button elUp = new JoystickButton(joy2, 2);
+    Button elDown = new JoystickButton(joy2, 3);
     Button openDoor = new JoystickButton(joy2, 4);
     Button closeDoor = new JoystickButton(joy2, 5);
-    
+    Joystick joy = new Joystick(1);
+    Button trigger = new JoystickButton(joy, 1);
+
     public OI() {
         elUp.whenPressed(new ElevatorUp());
         elDown.whenPressed(new ElevatorDown());
         openDoor.whenPressed(new OpenDoor());
-        closeDoor.whenPressed(new CloseDoor());            
-}
-    Joystick joy= new Joystick(1);
-    Button trigger = new JoystickButton(joy, 1);
-    
-    public OI(){
-        trigger.whileHeld(new DriveRotate()); 
+        closeDoor.whenPressed(new CloseDoor());
+        trigger.whileHeld(new DriveRotate());
     }
-    
+
     public double getX() {
         return joy.getX();
     }
+
     public double getY() {
         return joy.getY();
     }
-    
-}
 
+}
