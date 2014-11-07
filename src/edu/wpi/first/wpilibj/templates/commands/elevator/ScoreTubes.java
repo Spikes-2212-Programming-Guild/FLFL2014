d806a7f60765a8003d9ebede0ed82fd2b221b018
AtidSpikes <AtidSpikes@Spikes-PC>
2014-11-07 11:55:08 +0200

    Added commandGroup ScoreTubes

---
+++ src/edu/wpi/first/wpilibj/templates/commands/elevator/ScoreTubes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands.elevator;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author AtidSpikes
+ */
+public class ScoreTubes extends CommandGroup {
+
+    public ScoreTubes() {
+        addSequential(new ElevatorUp());
+        addSequential(new OpenDoor());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        addSequential(new CloseDoor());
+        addSequential(new ElevatorDown());
+    }
+}
