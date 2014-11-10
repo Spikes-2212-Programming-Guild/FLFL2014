4ab3567a28a80472ec3a60c32dd8f82bac7cd40e
Thinkredstone <ittai11@gmail.com>
2014-11-10 17:18:40 +0200

    Cleanup after merge

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+import edu.wpi.first.wpilibj.templates.commands.PIDdrive.AutonoumsCommand;
 
 
 /**
@@ -32,7 +33,7 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
-
+        autonomousCommand = new AutonoumsCommand();
         // Initialize all subsystems
         CommandBase.init();
     }
