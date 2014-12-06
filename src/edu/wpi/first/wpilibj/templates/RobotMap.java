0ea03d5fa70b1c8196e44af50e969b1cfd0c5dab
ThinkRedstone <ittai11@gmail.com>
2014-12-06 16:49:56 +0200

    ordered constants and added constants for arrivedLeft and arrivedRight

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -16,31 +16,37 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static final int rangefinderPort = 1;
     // public static final int rangefinderModule = 1;
+//    driveTrain
     public static final int FRONT_LEFT_TALON = 0;
     public static final int BACK_LEFT_TALON = 0;
     public static final int FRONT_RIGHT_TALON = 0;
     public static final int BACK_RIGHT_TALON = 0;
     public static final int DRIVE_ENCODER_1 = 0;
     public static final int DRIVE_ENCODER_2 = 0;
+    public static final int ARRIVED_LEFT_PORT = 0;
+    public static final int ARRIVED_RIGHT_PORT = 0;
     public static final double WHEEL_DIAMETER = 0;
 
-    public static final double AUTONOMOUS_DRIVE_DESTINATION = 0;
-
+//    elevator
     public static final int ELEVATOR_TOP_DI_PORT = 7;
     public static final int ELEVATOR_BOTTOM_DI_PORT = 8;
     public static final int ELEVATOR_LEFT_WHEEL_PORT = 5;
     public static final int ELEVATOR_RIGHT_WHEEL_PORT = 6;
+
+//    door
     public static final int DOOR_RELAY_PORT = 9;
     public static final int DOOR_TOP_DI_PORT = 10;
     public static final int DOOR_BOTTOM_DI_PORT = 11;
-
     public static final long SLEEP_BETWEEN_DOOR_OPEN_AND_CLOSE = 2000;
 
+//    driving PID
     public static final double DRIVE_PID_TOLERANCE = 0;
     public static final double DRIVER_KP = 0;
     public static final double DRIVER_KI = 0;
     public static final double DRIVER_KD = 0;
     public static final long DRIVER_DT = 0;
+    public static final double AUTONOMOUS_DRIVE_DESTINATION = 0;
+
     public static final int ENCODER_TICKS_IN_CYCLE = 0;
 
 }
